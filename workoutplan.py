41a589e7514881e91c38693ed02f1e8dbcdd46b7
Lori <lorianne.jack@gmail.com>
2026-05-19 09:45:28 -0400

    testing login

--- workoutplan.py
+++ workoutplan.py
@@ -31,134 +31,125 @@ NO = ":x:"
 KIND_OF = ":woman_shrugging:"
 
 # Assure the user is logged in and authorized
-if True:
-    print("logging in")
-# if not st.user.is_logged_in:
-#     login_screen()
-elif st.user.email not in st.secrets["authorized_users"]:
-    st.header(f"Access Denied {st.user.name}")
-    st.subheader(f"{st.user.email} does not have permission to view this app.")
-else:
-    st.header(f"Welcome, {st.user.name}!")
+
+
+
+st.header(f"Welcome, {st.user.name}!")
+st.session_state.current_user = st.user.given_name
+
+# Standard streamlit page configuration
+st.set_page_config(page_title='Mt. Baker Training Plan 2026',
+                    layout='wide',
+                    )
+
+# Get the dataframe for the trainingplan only
+if 'calendar_df' not in st.session_state:
+    st.session_state.calendar_df = get_calendar_dataframe()
+
+# Set the current user (based on login information)
+if 'current_user' not in st.session_state:
     st.session_state.current_user = st.user.given_name
 
-    # Standard streamlit page configuration
-    st.set_page_config(page_title='Mt. Baker Training Plan 2026',
-                       layout='wide',
-                      )
+if 'df_written_to_gsheets_text' not in st.session_state:
+    st.session_state.df_written_to_gsheets_text = 'Click Submit to write you workout completion status to google sheets'
 
-    # Get the dataframe for the trainingplan only
-    if 'calendar_df' not in st.session_state:
-        st.session_state.calendar_df = get_calendar_dataframe()
+# create tabs for the workout and displaying the entire schedule as a dataframe/table
+(current_workout_tab,
+    entire_schedule_tab,
+    workout_descriptions_tab) = st.tabs(['Workout',
+                                    'Entire Schedule',
+                                    'Workout Descriptions'])
 
-    # Set the current user (based on login information)
-    if 'current_user' not in st.session_state:
-        st.session_state.current_user = st.user.given_name
+# Current workout tab: displays the workout for selected date
+# Lets user set if they completed it or not.
+with current_workout_tab:
 
-    if 'df_written_to_gsheets_text' not in st.session_state:
-        st.session_state.df_written_to_gsheets_text = 'Click Submit to write you workout completion status to google sheets'
+    # display_date: whatever date is selected
+    if 'display_date' not in st.session_state:
+        st.session_state.display_date = datetime.now()
 
-    # create tabs for the workout and displaying the entire schedule as a dataframe/table
-    (current_workout_tab,
-     entire_schedule_tab,
-     workout_descriptions_tab) = st.tabs(['Workout',
-                                        'Entire Schedule',
-                                        'Workout Descriptions'])
+    # Version of the date that isn't ugly.
+    display_pretty = st.session_state.display_date.strftime('%A, %B %d, %Y')
 
-    # Current workout tab: displays the workout for selected date
-    # Lets user set if they completed it or not.
-    with current_workout_tab:
+    if 'display_date_text' not in st.session_state:
+        st.session_state.display_date_text = display_pretty
 
-        # display_date: whatever date is selected
-        if 'display_date' not in st.session_state:
-            st.session_state.display_date = datetime.now()
+    # version of the data the matches the dataframe date (also the index)
+    display_logical = st.session_state.display_date.strftime("%-m/%-d/%Y")
 
-        # Version of the date that isn't ugly.
-        display_pretty = st.session_state.display_date.strftime('%A, %B %d, %Y')
+    workout = get_workout(display_logical)
 
-        if 'display_date_text' not in st.session_state:
-            st.session_state.display_date_text = display_pretty
+    if 'workout_md' not in st.session_state:
+        st.session_state.workout_md = workout
 
-        # version of the data the matches the dataframe date (also the index)
-        display_logical = st.session_state.display_date.strftime("%-m/%-d/%Y")
+    if 'disable_prior_date_button' not in st.session_state:
+        st.session_state.disable_prior_date_button = False
+    if 'disable_next_date_button' not in st.session_state:
+        st.session_state.disable_next_date_button = False
 
-        workout = get_workout(display_logical)
-
-        if 'workout_md' not in st.session_state:
-            st.session_state.workout_md = workout
-
-        if 'disable_prior_date_button' not in st.session_state:
-            st.session_state.disable_prior_date_button = False
-        if 'disable_next_date_button' not in st.session_state:
-            st.session_state.disable_next_date_button = False
-
-        if 'completion_status_emoji' not in st.session_state:
-            completion_status = get_completion_status(display_logical)
-            set_completion_status_emoji(completion_status)
-
-
-         # Date Navigation Buttons and Header
-        prior_date_col, display_date_col, next_day_col, today_col = st.columns(4)
-        with prior_date_col:
-            st.button('See Prior Day\'s Workout',
-                      on_click=update_header, args=(1, 'prior'),
-                      disabled=st.session_state.disable_prior_date_button)
-        with display_date_col:
-            st.header(f"{st.session_state.completion_status_emoji} **Workout for {st.session_state.display_date_text}**")
-        with next_day_col:
-            st.button('See Next Day\'s Workout',
-                      on_click=update_header, args=(1, 'next'),
-                      disabled=st.session_state.disable_next_date_button)
-        with today_col:
-            st.button('Return to Today', on_click=update_header, args=(0, 'today'))
-
-
+    if 'completion_status_emoji' not in st.session_state:
         completion_status = get_completion_status(display_logical)
-        comment = get_comment(display_logical)
-
-        col_1, workout_col, col_3 = st.columns(3)
-        with col_1:
-            st.write("")
-        with workout_col:
-            st.markdown(st.session_state.workout_md)
-        with col_3:
-            st.write("")
-
-
-        completed = st.radio(
-            "Did you complete the workout?",
-            [f"Yes {YES}", f"No {NO}", f"Kinda {KIND_OF}"],
-        )
-        explaination = ""
-        if completed == f"Yes {YES}":
-            completion_status = "Completed"
-        elif completed == f"No {NO}":
-            completion_status = "Not Completed"
-        else:
-            completion_status = "Partially Completed"
-            explaination = st.text_area("Please explain what you were able to complete:")
         set_completion_status_emoji(completion_status)
-        with st.form("Submit Workout Status"):
-            submit_to_spreadsheet = st.form_submit_button("Submit Status")
-            if submit_to_spreadsheet:
-                st.session_state.calendar_df.loc[display_logical, f'{st.session_state.current_user} Completed'] = completion_status
-                st.session_state.calendar_df.loc[display_logical, f'{st.session_state.current_user} Comment'] = explaination
-                write_df_to_google_drive(st.session_state.calendar_df, completion_status=completion_status, comment=explaination)
-                update_header(date_delta=0)
-            st.write(st.session_state.df_written_to_gsheets_text)
-    with entire_schedule_tab:
-        st.markdown('# Currently Viewing the entire schedule')
-        st.table(st.session_state.calendar_df)
-    with workout_descriptions_tab:
-        st.markdown('# Workout Descriptions')
-        workout_descriptions_df = get_workout_descriptions_dataframe()
-        selected_workout = st.selectbox(label='Select a workout to view description:',
-                     options=workout_descriptions_df.index.tolist(),
-                     key='selected_workout_description')
 
-        filtered_df = workout_descriptions_df[workout_descriptions_df.index == selected_workout]
-        st.table(filtered_df)
 
-if st.user.is_logged_in:
-    if st.button("Log out"):
-        st.logout()
+        # Date Navigation Buttons and Header
+    prior_date_col, display_date_col, next_day_col, today_col = st.columns(4)
+    with prior_date_col:
+        st.button('See Prior Day\'s Workout',
+                    on_click=update_header, args=(1, 'prior'),
+                    disabled=st.session_state.disable_prior_date_button)
+    with display_date_col:
+        st.header(f"{st.session_state.completion_status_emoji} **Workout for {st.session_state.display_date_text}**")
+    with next_day_col:
+        st.button('See Next Day\'s Workout',
+                    on_click=update_header, args=(1, 'next'),
+                    disabled=st.session_state.disable_next_date_button)
+    with today_col:
+        st.button('Return to Today', on_click=update_header, args=(0, 'today'))
+
+
+    completion_status = get_completion_status(display_logical)
+    comment = get_comment(display_logical)
+
+    col_1, workout_col, col_3 = st.columns(3)
+    with col_1:
+        st.write("")
+    with workout_col:
+        st.markdown(st.session_state.workout_md)
+    with col_3:
+        st.write("")
+
+
+    completed = st.radio(
+        "Did you complete the workout?",
+        [f"Yes {YES}", f"No {NO}", f"Kinda {KIND_OF}"],
+    )
+    explaination = ""
+    if completed == f"Yes {YES}":
+        completion_status = "Completed"
+    elif completed == f"No {NO}":
+        completion_status = "Not Completed"
+    else:
+        completion_status = "Partially Completed"
+        explaination = st.text_area("Please explain what you were able to complete:")
+    set_completion_status_emoji(completion_status)
+    with st.form("Submit Workout Status"):
+        submit_to_spreadsheet = st.form_submit_button("Submit Status")
+        if submit_to_spreadsheet:
+            st.session_state.calendar_df.loc[display_logical, f'{st.session_state.current_user} Completed'] = completion_status
+            st.session_state.calendar_df.loc[display_logical, f'{st.session_state.current_user} Comment'] = explaination
+            write_df_to_google_drive(st.session_state.calendar_df, completion_status=completion_status, comment=explaination)
+            update_header(date_delta=0)
+        st.write(st.session_state.df_written_to_gsheets_text)
+with entire_schedule_tab:
+    st.markdown('# Currently Viewing the entire schedule')
+    st.table(st.session_state.calendar_df)
+with workout_descriptions_tab:
+    st.markdown('# Workout Descriptions')
+    workout_descriptions_df = get_workout_descriptions_dataframe()
+    selected_workout = st.selectbox(label='Select a workout to view description:',
+                    options=workout_descriptions_df.index.tolist(),
+                    key='selected_workout_description')
+
+    filtered_df = workout_descriptions_df[workout_descriptions_df.index == selected_workout]
+    st.table(filtered_df)
